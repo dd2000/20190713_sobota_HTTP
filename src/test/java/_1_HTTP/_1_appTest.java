9ca90f232cba5dd3957394fe798819ff6b3128c7
Dariusz.Żulpo <dd2000@wp.pl>
2019-07-13 12:35:54 +0200

    HTTP request

--- src/test/java/_1_HTTP/_1_appTest.java
+++ src/test/java/_1_HTTP/_1_appTest.java
@@ -2,9 +2,12 @@ package _1_HTTP;
 
 import org.apache.http.Header;
 import org.apache.http.StatusLine;
+import org.apache.http.client.entity.EntityBuilder;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.EntityTemplate;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -99,5 +102,44 @@ public class _1_appTest {
         System.out.println(responseAsString);
 
     } // test 3 - testDeleteSdAcademy()
+
+    // test 4 => wysyłanie danych (POST) na URI: http://sdacademy.pl
+    @Test
+    public void testSendDataToSdAcademy() throws IOException{
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost httpPost = new HttpPost("http://sdacademy.pl"); //  OK - status 200; status 405 - gdy URI: http://google.pl
+        //HttpPost httpPost = new HttpPost("http://google.pl"); //  OK - status 200; status 405 - gdy URI: http://google.pl
+
+        httpPost.setEntity(EntityBuilder.create()
+                            .setText("Jak się macie ?")
+                            .build());
+
+        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+
+        StatusLine statusLine = httpResponse.getStatusLine();
+        int statusCode = httpResponse.getStatusLine().getStatusCode();
+        String reasonPhrase = statusLine.getReasonPhrase();
+
+        System.out.println("Status: "+statusCode+", description: "+reasonPhrase);
+
+        Header[] allHeaders = httpResponse.getAllHeaders();
+        for (Header h: allHeaders
+        ) {
+            System.out.println("Header name: "+ h.getName()+", value:"+h.getValue());
+        }
+        if (statusCode<200 || statusCode>=300){
+            System.out.println("Coś poszło nie tak !");
+            return;
+        }
+        BasicResponseHandler basicResponseHandler = new BasicResponseHandler();
+        String responseAsString = basicResponseHandler.handleResponse(httpResponse);
+        System.out.println(responseAsString);
+
+    } // test 4 - testSendDataToSdAcademy()
+
+
+
 
 } // class _1_appTest
