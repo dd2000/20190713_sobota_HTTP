ecfb363f77bc233c1145a5a0e9d6bafbde37f09b
Dariusz.Żulpo <dd2000@wp.pl>
2019-07-13 15:35:56 +0200

    HTTP request

--- src/test/java/_1_HTTP/_1_appTest.java
+++ src/test/java/_1_HTTP/_1_appTest.java
@@ -11,7 +11,6 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.EntityTemplate;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
